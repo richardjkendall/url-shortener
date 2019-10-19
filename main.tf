02e23e09bab07705ee982f1186a40fd5842f245e
Richard Kendall <richard.kendall@gmail.com>
2019-10-19 14:36:30 +1100

    added recent changes

--- main.tf
+++ main.tf
@@ -121,6 +121,8 @@ resource "aws_lambda_function" "lambda" {
     role                = "${aws_iam_role.iam_for_lambda.arn}"
     handler             = "lambda_function.lambda_handler"
     runtime             = "python3.6"
+    memory_size         = "256"
+    timeout             = "10"
 
     environment {
         variables = {
@@ -183,7 +185,7 @@ resource "aws_api_gateway_integration" "lambda_root" {
 
 module "cors" {
   source = "github.com/squidfunk/terraform-aws-api-gateway-enable-cors"
-  
+
   api_id          = "${aws_api_gateway_rest_api.apigw.id}"
   api_resource_id = "${aws_api_gateway_rest_api.apigw.root_resource_id}"
 }
